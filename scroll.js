cb9bca74a7ee212eeda71cbd6f72116833052ad6
tertiux44 <secondsonofadebayo@gmail.com>
2022-12-30 18:54:23 +0100

    template and icons and divs

---
+++ scroll.js
@@ -0,0 +1,17 @@
+// I prefer to use this because scrollreveal causes a little site lag and has some bugs, plus this is easy to customize using html and css
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
